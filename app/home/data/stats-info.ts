8a7f82231434008ce51941b9bc007609f782aad4
Luis Pinto <dapin46@hotmail.com>
2026-03-23 18:59:20 -0500

    update: information of stats changed

--- app/home/data/stats-info.ts
+++ app/home/data/stats-info.ts
@@ -1,5 +1,5 @@
 export const statsInfo = [
   { important: 2, context: 'Años de experiencia' },
-  { important: '6+', context: 'Projectos completados' },
+  { important: '7+', context: 'Projectos completados' },
   { important: '100%', context: 'Arquitectura limpia' },
 ];
